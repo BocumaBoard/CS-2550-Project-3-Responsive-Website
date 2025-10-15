520811de651e5f12e5d62219e7bf58537404af4b
BocumaBoard <gearzoid2@gmail.com>
2025-10-15 23:52:10 +0000

    Refactored website sections to share same page, added javascript for toggling them on/off using menu links

--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
 
-// Function for using 'see more' button on homepage
-document.getElementById("gallery-btn").onclick = function() {
-  window.location.href = "gallery.html"
+// Variables/function for returning to homepage
+var mainSection = document.getElementById("main-section")
+var nHomeLink = document.getElementById("nav-home-link")
+var fHomeLink = document.getElementById("footer-home-link")
+
+function returnHome() {
+  mainSection.style.display = "flex"
+  imageGallery.style.display = "none"
 }
 
+nHomeLink.onclick = fHomeLink.onclick = returnHome 
+
+// Variables/function for opening image gallery
+var imageGallery = document.getElementById("gallery-grid-container")
+var galleryButton = document.getElementById("gallery-btn")
+var nGalleryLink = document.getElementById("nav-gallery-link")
+var fGalleryLink = document.getElementById("footer-gallery-link")
+
+function openGallery() {
+  mainSection.style.display = "none"
+  imageGallery.style.display = "grid"
+}
+
+nGalleryLink.onclick = fGalleryLink.onclick = galleryButton.onclick = openGallery
+
+// Variables/function for opening contact sidebar
 var nContactLink = document.getElementById("nav-contact-link")
 var fContactLink = document.getElementById("footer-contact-link")
 var contactPage = document.getElementById("contact-info-container")
 
-// Function for opening contact sidebar
+
 function openSidebar() {
   contactPage.classList.remove('fade-out') // Resets class list if set by previous click
   contactPage.style.display = "block"
