b27810f886cf842d1338ddd5405b9278a9016f41
BocumaBoard <gearzoid2@gmail.com>
2025-11-21 19:00:49 -0700

    Added skeleton for form page. Refactored CSS into additional files for better separation. Reorganized main.js.

--- js/main.js
+++ js/main.js
@@ -1,81 +1,107 @@
+// FIXME change indent size to 4 spaces
 
-// Variables/function for returning to homepage
+// ============================================================================
+// HOME SECTION
+// ============================================================================
 var mainSection = document.getElementById('main-section')
 var nHomeLink = document.getElementById('nav-home-link')
 var fHomeLink = document.getElementById('footer-home-link')
 
 function returnHome() {
-  mainSection.style.display = 'flex'
-  imageGallery.style.display = 'none'
+	mainSection.style.display = 'flex'
+	imageGallery.style.display = 'none'
+	visitorForm.style.display = 'none'
 }
 
-nHomeLink.onclick = fHomeLink.onclick = returnHome 
+nHomeLink.addEventListener('click', returnHome)
+fHomeLink.addEventListener('click', returnHome)
 
-// Variables/function for opening image gallery
+// ============================================================================
+// GALLERY SECTION
+// ============================================================================
 var imageGallery = document.getElementById('gallery-grid-container')
-var galleryButton = document.getElementById('gallery-btn')
-var nGalleryLink = document.getElementById('nav-gallery-link')
-var fGalleryLink = document.getElementById('footer-gallery-link')
+var galleryLinks = [ 
+		document.getElementById('gallery-btn'),
+		document.getElementById('nav-gallery-link'),
+		document.getElementById('footer-gallery-link')
+]
 
 function openGallery() {
-  mainSection.style.display = 'none'
-  imageGallery.style.display = 'grid'
+	imageGallery.style.display = 'grid'
+	mainSection.style.display = 'none'
+	visitorForm.style.display = 'none'
 }
 
-nGalleryLink.onclick = fGalleryLink.onclick = galleryButton.onclick = openGallery
+for (let elem of galleryLinks) {
+	elem.addEventListener('click', openGallery)
+}
 
-// Variables/function for opening contact sidebar
+// ============================================================================
+// CONTACT SECTION
+// ============================================================================
+var contactPage = document.getElementById('contact-info-container')
 var nContactLink = document.getElementById('nav-contact-link')
 var fContactLink = document.getElementById('footer-contact-link')
-var contactPage = document.getElementById('contact-info-container')
-
 
 function openSidebar() {
-  contactPage.classList.remove('fade-out') // Resets class list if set by previous click
-  contactPage.style.display = 'block'
+	contactPage.classList.remove('fade-out')
+	contactPage.style.display = 'block'
 }
 
-fContactLink.onclick = nContactLink.onclick = openSidebar
-  
-// Function for smoothly hiding contact info sidebar when clicked
+nContactLink.addEventListener('click', openSidebar)
+fContactLink.addEventListener('click', openSidebar)
+
+// Smoothly closes contact sidebar
 contactPage.addEventListener('click', function() {
-  const contactPage = document.getElementById('contact-info-container')
-
-  contactPage.classList.add('fade-out')
-
-  // Finishes hiding sidebar after transition
-  contactPage.addEventListener('transitionend', function handler() {
-    contactPage.style.display = 'none'
-    contactPage.removeEventListener('transitionend', handler)
-  })
+	contactPage.classList.add('fade-out')
+	contactPage.addEventListener('transitionend', function handler() {
+		contactPage.style.display = 'none'
+		contactPage.removeEventListener('transitionend', handler)
+	})
 })
 
-// Variables/function to change theme
+// ============================================================================
+// VISITOR FORM SECTION
+// ============================================================================
+var visitorForm = document.getElementById('visitor-form')
+var formHeaderLink = document.getElementById('nav-form')
+var footHeaderLink = document.getElementById('footer-form')
+
+function openVisitorForm() {
+	visitorForm.style.display = 'flex'
+	mainSection.style.display = 'none'
+	imageGallery.style.display = 'none'
+}
+
+formHeaderLink.addEventListener('click', openVisitorForm)
+footHeaderLink.addEventListener('click', openVisitorForm)
+
+// ============================================================================
+// THEME TOGGLE
+// ============================================================================
 var themeSelector = document.getElementById('nav-theme-toggle')
 var link = document.createElement('link')
 var toggled = false
 
+// Inserts theme stylesheet into head when called
 themeSelector.addEventListener('click', function() {
-  var siteHeader = document.querySelector('h1')
-  var headerImg = document.getElementById('header-image')
-  var head = document.head
-  
-  link.type = 'text/css'
-  link.rel = 'stylesheet'
-  link.href = 'css/theme.css'
+	var siteHeader = document.querySelector('h1')
+	var headerImg = document.getElementById('header-image')
+	var head = document.head
+	
+	link.type = 'text/css'
+	link.rel = 'stylesheet'
+	link.href = 'css/theme.css'
 
-  // Inserts theme sheet into head if untoggled when link is clicked, removes theme if toggled
-  if (!toggled) {
-    toggled = true
-    head.appendChild(link)
-    // Updates site header and image for theme
-    siteHeader.textContent = "Ben's (Spooky) Figure Gallery"
-    headerImg.src = 'img/tenko_sprite.png'
-  } else {
-    toggled = false
-    head.removeChild(link)
-    // Resets site header/image
-    siteHeader.textContent = "Ben's Figure Gallery"
-    headerImg.src = 'img/kaito_sprite.png'
-  } 
+	if (!toggled) {
+		toggled = true
+		head.appendChild(link)
+		siteHeader.textContent = "Ben's (Spooky) Figure Gallery"
+		headerImg.src = 'img/tenko_sprite.png'
+	} else {
+		toggled = false
+		head.removeChild(link)
+		siteHeader.textContent = "Ben's Figure Gallery"
+		headerImg.src = 'img/kaito_sprite.png'
+	} 
 })
